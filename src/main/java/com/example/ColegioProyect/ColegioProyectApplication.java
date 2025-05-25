13f22ea9bf9eadac88908df5cc3adddbb692cb2f
EliasNoble6 <20233tn068@utez.edu.mx>
2025-05-25 10:45:17 -0600

    Primer comentario

---
+++ src/main/java/com/example/ColegioProyect/ColegioProyectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ColegioProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColegioProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColegioProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ColegioProyect/ColegioProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ColegioProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColegioProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
